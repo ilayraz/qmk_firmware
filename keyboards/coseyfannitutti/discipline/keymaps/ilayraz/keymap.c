f521f050e0b8081a70bcfb1719e80b5c2dfc969c
Ilay Raz <ilay.raz@gmail.com>
2021-08-09 21:06:48 -0700

    Update

--- keyboards/coseyfannitutti/discipline/keymaps/ilayraz/keymap.c
+++ keyboards/coseyfannitutti/discipline/keymaps/ilayraz/keymap.c
@@ -1,37 +1,34 @@
 #include QMK_KEYBOARD_H
 #include "keymap.h"
 
-#define TAPPING_TERM 200
 #define RAPID_A_CD 300
+#define TAPPING_TERM 200
 
-extern keymap_config_t keymap_config;
+#define PRESS_ELSE(state, tap, press) if (state->pressed) return tap; else return press;
 
-int cur_dance(qk_tap_dance_state_t *state);
 void alt_finished(qk_tap_dance_state_t *state, void *user_data);
 void alt_reset(qk_tap_dance_state_t *state, void *user_data);
 
-typedef struct {
-    int state;
-    bool is_press_action;
-} tap;
+extern keymap_config_t keymap_config;
 
 typedef struct {
     uint16_t timer;
     bool is_pressed;
 } key_timer;
 
-static tap alttap_state = {
-    .is_press_action = true,
-    .state = 0
+enum dance_actions {
+    TD_ALT
 };
 
-static key_timer rapid_a_state = {
-    .timer = 0,
-    .is_pressed = false
+enum custom_keycodes {
+    R_A = SAFE_RANGE,
+    LCTRL,
+    RCTRL
 };
 
 enum taps {
-    SINGLE_TAP = 1,
+    UNDEF,
+    SINGLE_TAP,
     SINGLE_HOLD,
     DOUBLE_TAP,
     DOUBLE_HOLD,
@@ -39,26 +36,31 @@ enum taps {
     TRIPLE_HOLD,
 };
 
-enum dance_actions {
-    TD_ALT,
-};
-
-enum custom_keycodes {
-    R_A = SAFE_RANGE,
-};
-
 enum layers_idx {
     _BLQ, // Base Layer
     _FL, // Functions
 };
 
+static key_timer rapid_a_state = {
+    .timer = 0,
+    .is_pressed = false
+};
+static uint16_t timer_lctrl;
+static uint16_t timer_rctrl;
+static enum taps taps_alt = UNDEF;
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_ALT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, alt_finished, alt_reset)
+};
+
+// RCMD_T(KC_RBRC)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BLQ] = LAYOUT_ilayraz(
      KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV,
      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL,
      KC_LCTRL, R_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGUP,
      KC_LSPO, KC_BSLS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, KC_UP, KC_PGDN,
-     KC_LCTL, KC_LGUI, TD(TD_ALT), KC_SPC, KC_RCTL, KC_RGUI, KC_LEFT, KC_DOWN, KC_RIGHT),
+     LCTRL, LCMD_T(KC_LBRC), TD(TD_ALT), KC_SPC, RCTRL, L_CMD, KC_LEFT, KC_DOWN, KC_RIGHT),
 
     [_FL] = LAYOUT_ilayraz(
      KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_MSTP, KC_MPLY,
@@ -68,10 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TRNS, KC_TRNS, TG(_FL), KC_ENT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE)
 };
 
-qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_ALT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, alt_finished, alt_reset)
-};
-
+// Called every key press/release
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     case R_A:
@@ -80,11 +79,35 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         if (rapid_a_state.is_pressed) tap_code(KC_A);
         break;
+    case LCTRL:
+        if (record->event.pressed) {
+            timer_lctrl = timer_read();
+            register_code(KC_LCTRL);
+        } else {
+            unregister_code(KC_LCTRL);
+            if (timer_elapsed(timer_lctrl) < TAPPING_TERM)
+                SEND_STRING("{");
+        }
+        break;
+    case RCTRL:
+        if (record->event.pressed) {
+            timer_rctrl = timer_read();
+            register_code(KC_RCTRL);
+        } else {
+            unregister_code(KC_RCTRL);
+            if (timer_elapsed(timer_rctrl) < TAPPING_TERM)
+                SEND_STRING("}");
+        }
+        break;
+    case KC_ESC:
+        clear_keyboard();
+        break;
     }
 
     return true;
 }
 
+// Called every scan (often)
 void matrix_scan_user() {
     if (rapid_a_state.is_pressed) {
         if (timer_elapsed(rapid_a_state.timer) > RAPID_A_CD) {
@@ -94,27 +117,27 @@ void matrix_scan_user() {
     }
 }
 
-int cur_dance(qk_tap_dance_state_t *state) {
+/**
+ * TAP DANCE functions
+ **/
+
+enum taps cur_dance(qk_tap_dance_state_t *state) {
     switch (state->count) {
     case 1:
-        if (state->pressed) return SINGLE_HOLD;
-        else return SINGLE_TAP;
-        break;
+        PRESS_ELSE(state, SINGLE_HOLD, SINGLE_TAP);
     case 2:
-        if (state->pressed) return DOUBLE_HOLD;
-        else return DOUBLE_TAP;
-        break;
+        PRESS_ELSE(state, DOUBLE_HOLD, DOUBLE_TAP);
     case 3:
-        if (state->pressed) return TRIPLE_HOLD;
-        else return TRIPLE_TAP;
-        break;
+        PRESS_ELSE(state, TRIPLE_HOLD, TRIPLE_TAP);
+    default:
+        return UNDEF;
     }
-    return -1;
 }
 
 void alt_finished(qk_tap_dance_state_t *state, void *user_data) {
-    alttap_state.state = cur_dance(state);
-    switch(alttap_state.state) {
+    taps_alt = cur_dance(state);
+
+    switch(taps_alt) {
     case SINGLE_TAP:
         set_oneshot_layer(_FL, ONESHOT_START);
         clear_oneshot_layer_state(ONESHOT_PRESSED);
@@ -131,11 +154,12 @@ void alt_finished(qk_tap_dance_state_t *state, void *user_data) {
         register_code(KC_LALT);
         layer_on(_FL);
         break;
+    default: break;
     }
 }
 
 void alt_reset (qk_tap_dance_state_t *state, void *user_data) {
-    switch (alttap_state.state) {
+    switch (taps_alt) {
     case SINGLE_HOLD: unregister_code(KC_LALT); break;
     case DOUBLE_TAP:
     case DOUBLE_HOLD: layer_off(_FL); break;
@@ -143,5 +167,6 @@ void alt_reset (qk_tap_dance_state_t *state, void *user_data) {
         layer_off(_FL);
         unregister_code(KC_LALT);
         break;
+    default: break;
     }
 }
